348ee8112b2adbfc962ef7c9c6ea40d027bc0e51
WisnuWahyudi <wisnumarine2@gmail.com>
2023-07-10 20:07:06 +0700

    update activity with data

---
+++ app/src/main/java/com/domainpt/myintentapp/activity/MoveWithDataActivity.kt
@@ -0,0 +1,12 @@
+package com.domainpt.myintentapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.domainpt.myintentapp.R
+
+class MoveWithDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+    }
+}
